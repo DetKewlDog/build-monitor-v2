50190fb126aa190046378cc729e5eaf4df8ae1e7
Bar Goldenstein <bargolden12@gmail.com>
2023-06-08 12:01:46 +0300

    implemented Flask-based server

---
+++ server/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import monitor as Monitor
+
+app = Flask(__name__)
+
+# This code acts as a sort of API server
+# This will allow to communicate with it using javascript
+
+# Yes, I could've just used this Flask app as the monitor
+# But then I'd need to refresh the Flask app every time I want to display new data
+# (that's the only method I am aware of. Flask apps are static pages)
+
+# Besides, I want to use React :D
+
+@app.route('/')
+def index():
+    return Monitor.get_msg()
+
+
+if __name__ == '__main__':
+    Monitor.init()
+    app.run(host='0.0.0.0')
